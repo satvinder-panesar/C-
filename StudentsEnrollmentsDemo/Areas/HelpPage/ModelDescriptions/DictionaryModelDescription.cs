f99740f34b7cc04c98926b05e2e61630d601c2f0
Panesar, Satvinder Singh (US - Dallas) <spanesar@deloitte.com>
2020-02-29 13:28:51 -0600

    Add project files.

---
+++ StudentsEnrollmentsDemo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace StudentsEnrollmentsDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ StudentsEnrollmentsDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StudentsEnrollmentsDemo.WebApiApplication" Language="C#" %>
