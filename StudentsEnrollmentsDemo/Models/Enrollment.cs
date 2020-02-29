31557f280693285dbb7bbc5b4f30bfe821cc9048
Panesar, Satvinder Singh (US - Dallas) <spanesar@deloitte.com>
2020-02-29 13:38:19 -0600

    adding models

---
+++ StudentsEnrollmentsDemo/Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsEnrollmentsDemo.Models
+{
+    public enum Grade
+    {
+        A, B, C, D
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
