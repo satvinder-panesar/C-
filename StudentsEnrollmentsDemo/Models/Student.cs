282d44d86aae99306adbf42f2f1cccb7769d701f
Panesar, Satvinder Singh (US - Dallas) <spanesar@deloitte.com>
2020-03-01 15:48:58 -0600

    removing virtual keyword

--- StudentsEnrollmentsDemo/Models/Student.cs
+++ StudentsEnrollmentsDemo/Models/Student.cs
@@ -11,6 +11,6 @@ namespace StudentsEnrollmentsDemo.Models
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public DateTime EnrollmentDate { get; set; }
-        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
